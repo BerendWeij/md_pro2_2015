3b36921a4b9ee62b3d7f40ca536909e5a638258b
Berend Weij <info@berendweij.nl>
2015-11-09 11:38:32 +0100

    added comments

--- les_1/js/main.js
+++ les_1/js/main.js
@@ -1,11 +1,12 @@
 window.addEventListener("load", function(){
 
+    /** we halen het HTML element op waar we iets mee willen doen */
     var notificationBar = document.querySelector(".notification-bar");
 
-    console.log(notificationBar.classList);
-
+    /** we luisteren of er op de webpagina wordt geklikt */
     document.addEventListener("click", toggleNotificationBar);
 
+    /** als er op de pagina wordt geklikt: toggle de class enabled */
     function toggleNotificationBar(){
         notificationBar.classList.toggle("enabled");
     }
